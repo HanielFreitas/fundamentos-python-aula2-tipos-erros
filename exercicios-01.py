6869c2449084541cd92d4f36c0482f1c80950dd0
HanielFreitas <hanielfreitas3@gmail.com>
2025-06-14 19:45:07 -0400

    feat: soma dois números inteiros informados pelo usuário

---
+++ exercicios-01.py
@@ -0,0 +1,16 @@
+"""
+Inteiros (int)
+1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+"""
+# Exercicio 1: Soma de dois números inteiros
+print("\nVAMOS TREINAR SOMA DE DOIS NUMEROS INTEIROS?")
+print("************************************************\n")
+num1 = int(input("Digite o primeiro número inteiro: "))
+num2 = int(input("Digite o segundo número inteiro: "))
+soma = num1 + num2
+print(f"A soma dos números é: {soma}")
+
