73c2ce2428316e640a8cb487c44f4542ae9c1631
HanielFreitas <hanielfreitas3@gmail.com>
2025-06-14 23:11:14 -0400

    iniciando exercicio04

---
+++ exercicios-04.py
@@ -0,0 +1,12 @@
+# Booleanos (bool)
+# Escreva um programa que avalie duas expressões booleanas inseridas pelo usuário e retorne o resultado da operação AND entre elas.
+# Crie um programa que receba dois valores booleanos do usuário e retorne o resultado da operação OR.
+# Desenvolva um programa que peça ao usuário para inserir um valor booleano e, em seguida, inverta esse valor.
+# Faça um programa que compare se dois números fornecidos pelo usuário são iguais.
+# Escreva um programa que verifique se dois números fornecidos pelo usuário são diferentes.
+
+def titulo(titulo):
+    print("\n" + titulo.upper())
+    print("*" * len(titulo) + "\n")
+
+
