1f4ebc067695564cc851dfe4363fb3cb1d4dda96
HanielFreitas <hanielfreitas3@gmail.com>
2025-06-14 23:17:16 -0400

    Resultado da logica booleana com AND

--- exercicios-04.py
+++ exercicios-04.py
@@ -1,12 +1,17 @@
 # Booleanos (bool)
-# Escreva um programa que avalie duas expressões booleanas inseridas pelo usuário e retorne o resultado da operação AND entre elas.
-# Crie um programa que receba dois valores booleanos do usuário e retorne o resultado da operação OR.
-# Desenvolva um programa que peça ao usuário para inserir um valor booleano e, em seguida, inverta esse valor.
-# Faça um programa que compare se dois números fornecidos pelo usuário são iguais.
-# Escreva um programa que verifique se dois números fornecidos pelo usuário são diferentes.
+# 16. Escreva um programa que avalie duas expressões booleanas inseridas pelo usuário e retorne o resultado da operação AND entre elas.
+# 17. Crie um programa que receba dois valores booleanos do usuário e retorne o resultado da operação OR.
+# 18. Desenvolva um programa que peça ao usuário para inserir um valor booleano e, em seguida, inverta esse valor.
+# 19. Faça um programa que compare se dois números fornecidos pelo usuário são iguais.
+# 20. Escreva um programa que verifique se dois números fornecidos pelo usuário são diferentes.
 
 def titulo(titulo):
     print("\n" + titulo.upper())
     print("*" * len(titulo) + "\n")
 
+# 16. Escreva um programa que avalie duas expressões booleanas inseridas pelo usuário e retorne o resultado da operação AND entre elas.
 
+verdade = True
+Falso = False
+resultado = verdade and Falso
+print(f"Resultado do AND lógica: {resultado}")
